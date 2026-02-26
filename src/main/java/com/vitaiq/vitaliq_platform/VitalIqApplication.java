a2cfcfc593cee081ecd70cde2e61e904d81a489e
Shravya sri Vuppala <shravyasrivuppala@gmail.com>
2026-02-25 17:47:37 -0800

    feat: initial VitalIQ platform setup with Spring Boot 4.0, Kafka, Elasticsearch, OpenAI

---
+++ src/main/java/com/vitaiq/vitaliq_platform/VitalIqApplication.java
@@ -0,0 +1,13 @@
+package com.vitaiq.vitaliq_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalIqApplication.class, args);
+	}
+
+}
